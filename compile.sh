1da738ee4b1cc2963841b49248df02849e4b5723
gtxaspec <gtxent@gmail.com>
2022-07-08 18:27:47 -0700

    add soundcard_block

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+CC=/home/turismo/toolchain/mips-gcc472-glibc216-64bit-master/bin/mips-linux-uclibc-gnu-gcc make; md5sum libcallback.so
---
+++ soundcard_block.c
@@ -0,0 +1,12 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
+
+sdk_device_monitor_soundcard_loop(void) {
+		//printf("disable soundcard\n");
+		return 0;
+}
+
