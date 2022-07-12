8706c559206b1ea2414de3ee0fc196e6b0630f66
gtxaspec <gtxent@gmail.com>
2022-07-11 22:31:37 -0700

    add timelapse function to cmd

--- command.c
+++ command.c
@@ -23,6 +23,7 @@ extern char *IrLed(int fd, char *tokenPtr);
 extern char *AudioPlay(int fd, char *tokenPtr);
 extern char *CurlDebug(int fd, char *tokenPtr);
 extern char *mp4Write(int fd, char *tokenPtr);
+extern char *Timelapse(int fd, char *tokenPtr);
 extern char *imp_Control(int fd, char *tokenPtr);
 
 struct CommandTableSt {
@@ -40,6 +41,7 @@ struct CommandTableSt CommandTable[] = {
   { "aplay",      &AudioPlay },
   { "curlDebug",  &CurlDebug },
   { "mp4write",      &mp4Write },
+  { "timelapse",  &Timelapse },
   { "imp_control",      &imp_Control },
 
 };
