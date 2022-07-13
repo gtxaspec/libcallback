29876183e0ee34b4c316f07bcc00f869b795005f
gtxaspec <gtxent@gmail.com>
2022-07-12 23:26:51 -0700

    update audio_play.c to latest version

--- audio_play.c
+++ audio_play.c
@@ -1,17 +1,21 @@
+#include <dlfcn.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-extern void local_sdk_speaker_set_pa_mode(int mode);
-extern void local_sdk_speaker_set_ap_mode(int mode);
-extern void local_sdk_speaker_clean_buf_data();
-extern void local_sdk_speaker_set_volume(int volume);
+extern int local_sdk_speaker_clean_buf_data();
+extern int local_sdk_speaker_set_volume(int volume);
 extern int local_sdk_speaker_feed_pcm_data(unsigned char *buf, int size);
-extern void local_sdk_speaker_finish_buf_data();
+extern int local_sdk_speaker_set_ap_mode(int mode);
+extern int local_sdk_speaker_set_pa_mode(int mode);
+extern int *get_speaker_params();
+extern int get_speaker_params_run_state();
+extern int IMP_AO_QueryChnStat(unsigned int a, unsigned int b, int *buf);
 extern void CommandResponse(int fd, const char *res);
 
+static int (*set_pa_mode)(int mode);
 static pthread_mutex_t AudioPlayMutex = PTHREAD_MUTEX_INITIALIZER;
 static int AudioPlayFd = -1;
 static char waveFile[256];
@@ -28,54 +32,48 @@ int PlayPCM(char *file, int vol) {
     0x02, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61
   };
 
-  fprintf(stderr, "[command] [audio_play.c] file:%s\n", file);
+  printf("[command] [audio_play.c] aplay: file:%s\n", file);
   FILE *fp = fopen(file, "rb");
   if(fp == NULL) {
-    fprintf(stderr, "[command] [audio_play.c] fopen %s failed!\n", file);
+    fprintf(stderr, "[command] [audio_play.c] aplay err: fopen %s failed!\n", file);
     return -1;
   } else {
     size_t size = fread(buf, 1, waveHeaderLength, fp);
     if(size != waveHeaderLength) {
-      fprintf(stderr, "[command] [audio_play.c] header size error\n");
+      fprintf(stderr, "[command] [audio_play.c] aplay err: header size error\n");
+      //If the header size is wrong, try to play anyway
     }
     buf[4] = buf[5] = buf[6] = buf[7] = 0;
     if(memcmp(buf, cmp, waveHeaderLength - 4)) {
-      fprintf(stderr, "[command] [audio_play.c] header error\n");
+      fprintf(stderr, "[command] [audio_play.c] aplay err: header error\n");
+      //If the header is wrong, try to play anyway
     }
     local_sdk_speaker_clean_buf_data();
     local_sdk_speaker_set_volume(vol);
-
- if(!local_sdk_speaker_set_pa_mode) {
-    local_sdk_speaker_set_ap_mode(3);
-      fprintf(stderr, "[command] [audio_play.c] set ap mode 3\n");
-  }
-
- if(!local_sdk_speaker_set_ap_mode) {
-    local_sdk_speaker_set_pa_mode(3);
-      fprintf(stderr, "[command] [audio_play.c] set pa mode 3\n");
-  }
-
-
-
+    set_pa_mode(3);
+      //Ignore the chunks
+    //int chunkSize = (buf[43] << 24) | (buf[42] << 16) | (buf[41] << 8) | buf[40];
     while(!feof(fp)) {
       size = fread(buf, 1, bufLength, fp);
       if (size <= 0) break;
+      //if(size > chunkSize) size = chunkSize;
+      //chunkSize -= size;
       while(local_sdk_speaker_feed_pcm_data(buf, size)) usleep(100 * 1000);
     }
     fclose(fp);
+    int *params = get_speaker_params();
+    while(1) {
+      int runState = get_speaker_params_run_state();
+      if(runState != 3) break;
+      int buf[3];
+      int stat = IMP_AO_QueryChnStat(params[7], params[8], buf);
+      if(stat || !buf[2]) break;
+      usleep(100 * 1000);
+    }
+    //Increase the sleep so the file doesn't abruptly end early
     usleep(2 * 1000 * 1000);
-    local_sdk_speaker_finish_buf_data();
-    local_sdk_speaker_set_volume(0);
-
- if(!local_sdk_speaker_set_pa_mode) {
-    local_sdk_speaker_set_ap_mode(0);
+    set_pa_mode(0);
   }
- if(!local_sdk_speaker_set_ap_mode) {
-    local_sdk_speaker_set_pa_mode(0);
-  }
-
-  }
-  fprintf(stderr, "[command] [audio_play.c] finish\n");
   return 0;
 }
 
@@ -94,14 +92,19 @@ static void *AudioPlayThread() {
 char *AudioPlay(int fd, char *tokenPtr) {
 
   if(AudioPlayFd >= 0) {
-    fprintf(stderr, "[command] [audio_play.c] Previous file is still playing. %d %d\n", AudioPlayFd, fd);
-    return "[command] [audio_play.c] error";
+    fprintf(stderr, "[command] [audio_play.c] aplay err: Previous file is still playing. %d %d\n", AudioPlayFd, fd);
+    return "error";
+  }
+
+  if(!set_pa_mode) {
+    fprintf(stderr, "[command] [audio_play.c] aplay err: local_sdk_speaker_set_[ap]_mode not found.\n", AudioPlayFd, fd);
+    return "error";
   }
 
   char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
   if(!p) {
-    fprintf(stderr, "[command] [audio_play.c] usage : aplay <wave file> [<volume>]\n");
-    return "[command] [audio_play.c] error";
+    fprintf(stderr, "[command] [audio_play.c] aplay err: usage : aplay <wave file> [<volume>]\n");
+    return "error";
   }
   strncpy(waveFile, p, 255);
 
@@ -116,6 +119,8 @@ char *AudioPlay(int fd, char *tokenPtr) {
 
 static void __attribute ((constructor)) AudioPlayInit(void) {
 
+  set_pa_mode = local_sdk_speaker_set_ap_mode;
+  if(!set_pa_mode) set_pa_mode = local_sdk_speaker_set_pa_mode;
   pthread_mutex_lock(&AudioPlayMutex);
   pthread_t thread;
   if(pthread_create(&thread, NULL, AudioPlayThread, NULL)) {
---
+++ platform.h
@@ -0,0 +1,8 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+extern const char *nd_enable;
+extern const char *product_T31;
+extern const char *product_T20;
+extern const char *product_DB3;
+extern const char *enableMotorStop;
+#endif
